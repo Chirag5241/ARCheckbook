d85c984bc5fa726d686f55ccfd094c08613253fc
Not MANav <manav2modi@gmail.com>
2020-03-01 05:05:41 -0600

    final test one bro

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import os, io, sys
 from google.cloud import vision
 from google.cloud.vision import types
-import pandas as pd 
+import pandas as pd
 
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'C:/Users/Dinesh/VisionAPIDemo/ServiceAccountToken.json'
 
@@ -9,7 +9,7 @@ client = vision.ImageAnnotatorClient()
 
 def detectText(img):
 
-    with io.open(os.path.join(FOLDER_PATH, FILE_NAME), 'rb') as image_file:
+    with io.open(img, 'rb') as image_file:
         content = image_file.read()
 
     image = vision.types.Image(content=content)
@@ -20,14 +20,17 @@ def detectText(img):
     for text in texts:
         df = df.append(
                 dict(
-                    locale=text.locale,
+                    locale='en',
                     description=text.description
                     ),
                 ignore_index=True
             )
         return df
 
-FILE_NAME = sys.argv[1]
-FOLDER_PATH = r'C:\Users\Dinesh\VisionAPIDemo\pics'
-print(detectText(os.path.join(FOLDER_PATH, FILE_NAME)))
-
+#FILE_NAME = sys.argv[1]
+#FOLDER_PATH = r'C:\Users\Dinesh\VisionAPIDemo\pics'
+output = detectText("hey4.jpg")
+print(output['description'].array[0])
+def check_info():
+    a = str(output['description'].array[0])
+    return (a)
